575dbcb53e9fbdf6dd85bc0136355aa220a54050
Kan Haoning <80746062+kanhaoning@users.noreply.github.com>
2025-07-13 17:01:45 +0800

    Delete Reranker-Distillation directory

--- Reranker-Distillation/create_triplets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 将原始 logits 数据转换为 MarginMSE 格式
-python create_triplets.py \
-  --input_files train_distill_qwen3_8b_vLLMlogit.jsonl test_distill_qwen3_8b_vLLMlogit.jsonl \
-  --output_suffix _margin_sampled \
-  --top_k 8 \
-  --num_negatives 4
